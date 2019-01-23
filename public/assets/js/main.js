8578b43ac50aa09e73c8887883b0ccfa70bacf11
Michelle Poplin <mlip6547@gmail.com>
2019-01-22 20:40:24 -0500

    updated main js function

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,25 @@
 // JS TO GRAB FORM VALUES AND SEND TO API ROUTES
+
+$(document).on("click", "button.submit", createNewRace);
+
+function createNewRace(event) {
+    event.preventDefault();
+    var newRace = {
+        firstName: $('#firstName').val().trim(),
+        lastName: $('#lastName').val().trim(),
+        raceName: $('#raceName').val().trim(),
+        category: $('#category').val().trim(),
+        street: $('#street').val().trim(),
+        city: $('#city').val().trim(),
+        state: $('#state').val().trim(),
+        zip: $('#zip').val().trim(),
+        phone: $('#phone').val().trim(),
+        email: $('#email').val().trim(),
+        url: $('#url').val().trim(),
+        raceLength: $('#raceLength').val().trim(),
+        description: $('#description').val().trim(),
+        participantCap: $('#participantCap').val().trim(),
+    };
+
+    $.post("/api/races", newRace);
+}
