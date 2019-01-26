2107dd7738845842ce691a69d1f4f04a751bb767
Chad Cook <chadcookcoding@gmail.com>
2019-01-26 11:28:11 -0500

    moved carousel to the external js file

--- public/assets/js/raceFilters.js
+++ public/assets/js/raceFilters.js
@@ -69,19 +69,21 @@ $(document).ready(function() {
   // *******************************************************
   //                CAROUSEL JAVASCRIPT
   // *******************************************************
-//   var slideIndex = 0;
-//   carousel();
-//   function carousel() {
-//     var i;
-//     var x = document.getElementsByClassName("sportslide");
-//     for (i = 0; i < x.length; i++) {
-//       x[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > x.length) {
-//       slideIndex = 1
-//     }
-//     x[slideIndex - 1].style.display = "block";
-//     setTimeout(carousel, 4000); // Change image every 4 seconds
-//   }
+    // Carousel code
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+      var i;
+      var x = document.getElementsByClassName("sportslide");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > x.length) {
+        slideIndex = 1
+      }
+      x[slideIndex - 1].style.display = "block";
+      setTimeout(carousel, 4000); // Change image every 4 seconds
+    }
 })
